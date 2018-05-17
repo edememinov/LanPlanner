86e712e4b5afed0943e1cf55fff319358d4f9111
Edem <edememinov@hotmail.com>
2018-05-17 16:43:05 +0200

    Added bot framework

---
+++ PlannerLanPartyBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=plannerlanpartybot-Web-Deploy.pubxml -p:Password=eCHCmgYhuJljwf0j6kWMmwageu7oTRH86KA22gE6NCeogCKlrrufLBaTXDeW
+
